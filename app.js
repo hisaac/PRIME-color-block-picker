5635012703614ebbab4ea1ed19ae92db1b759f0e
Isaac Halvorson <hello@hisaac.net>
2016-11-02 08:15:15 -0500

    successfully completed normal mode

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
 $(document).ready(function(){
-//-------------------------------- VARIABLES ---------------------------------//
+//----------------------------- GLOBAL VARIABLES -----------------------------//
 
-
+    var randomColor = null;
 
 //---------------------------------- LOGIC -----------------------------------//
 
-    // $('#blockContainer').append(
-    //     '<div class="colorBlock" id="red"></div>' +
-    //     '<div class="colorBlock" id="green"></div>' +
-    //     '<div class="colorBlock" id="yellow"></div>' +
-    //     '<div class="colorBlock" id="blue"></div>'
-    // );
+    // append color divs to the DOM
+    $('#blockContainer').append(
+        '<div class="colorBlock" id="red"></div>' +
+        '<div class="colorBlock" id="green"></div>' +
+        '<div class="colorBlock" id="yellow"></div>' +
+        '<div class="colorBlock" id="blue"></div>'
+    );
+
+    // calls multiple functions 
+    $('.colorBlock').click(function(){
+        appendResult(compareRandClicked(numberToColor(randomNumber(1, 4)), this));
+    });
 
 //-------------------------------- FUNCTIONS ---------------------------------//
 
     // random number generator
     function randomNumber(min, max){
         return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+
+    // converts number to specific div element
+    function numberToColor(number){
+        switch (number) {
+            case 1:
+                return red;
+                break;
+            case 2:
+                return green;
+                break;
+            case 3:
+                return yellow;
+                break;
+            case 4:
+                return blue;
+                break;
+        }
+    }
+
+    // compares randomly selected color to clicked color
+    function compareRandClicked (random, clicked) {
+        if (random === clicked){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // appends correct or incorrect to the DOM
+    function appendResult (boolean) {
+        if (boolean) {
+            $('body').append('<h2>Correct! :)</h2>');
+        } else {
+            $('body').append('<h2>Incorrect! :(</h2>');
+        }
     }
 
 });
