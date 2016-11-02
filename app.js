258b5d32989913bd6b4c210fd1e1eaae71a6c028
Isaac Halvorson <hello@hisaac.net>
2016-11-01 20:36:03 -0500

    try to color divs

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-$.(document).ready(function(){
-    
+$(document).ready(function(){
+//-------------------------------- VARIABLES ---------------------------------//
+
+
+
+//---------------------------------- LOGIC -----------------------------------//
+
+    $('.colors').append(
+        '<div id="red"></div>' +
+        '<div id="green"></div>' +
+        '<div id="yellow"></div>' +
+        '<div id="blue"></div>'
+    );
+
+//-------------------------------- FUNCTIONS ---------------------------------//
+
+    // random number generator
+    function randomNumber(min, max){
+        return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+
 });
